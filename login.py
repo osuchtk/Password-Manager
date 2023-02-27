85649816333c854f9e7d4fa1a4beab663c5ab666
osuchtk <osiak.wojciechx@gmail.com>
2023-02-27 22:28:37 +0100

    Making code easier to read
    Added comments and deleted unnecessary spaces near "=" symbols

--- login.py
+++ login.py
@@ -12,11 +12,14 @@ from kivy.core.window import Window
 from manager import MainScreen
 
 kivy.require('2.1.0')
-#abcd
-#1234
+
+
+# abcd
+# 1234
 
 
 class Login(FloatLayout):
+    # constructor preparing view showed when application is launched
     def __init__(self):
         super(Login, self).__init__()
         # declaring elements
@@ -24,60 +27,59 @@ class Login(FloatLayout):
         self.key = None
 
         # label text
-        self.add_widget(Label(text = "Password Manager",
-                              size_hint = (1, 1),
-                              pos = (0, 120),
-                              bold = True,
-                              font_size = 20,
-                              underline = True))
+        self.add_widget(Label(text="Password Manager",
+                              size_hint=(1, 1),
+                              pos=(0, 120),
+                              bold=True,
+                              font_size=20,
+                              underline=True))
         # username label and text field
-        self.add_widget(Label(text = "Login",
-                              size_hint = (1, 1),
-                              pos = (-130, 50)))
-        self.username = TextInput(multiline = False,
-                                  size_hint = (None, None),
-                                  pos = (200, 200),
-                                  width = 200,
-                                  height = 30,
-                                  write_tab = False)
+        self.add_widget(Label(text="Login",
+                              size_hint=(1, 1),
+                              pos=(-130, 50)))
+        self.username = TextInput(multiline=False,
+                                  size_hint=(None, None),
+                                  pos=(200, 200),
+                                  width=200,
+                                  height=30,
+                                  write_tab=False)
         self.add_widget(self.username)
 
         # password label and text field
-        self.add_widget(Label(text = "Password",
-                              size_hint = (1, 1),
-                              pos = (-130, -10)))
-        self.password = TextInput(multiline = False,
-                                  password = True,
-                                  size_hint =(None, None),
-                                  pos = (200, 140),
-                                  width = 200,
-                                  height = 30,
-                                  write_tab = False)
+        self.add_widget(Label(text="Password",
+                              size_hint=(1, 1),
+                              pos=(-130, -10)))
+        self.password = TextInput(multiline=False,
+                                  password=True,
+                                  size_hint=(None, None),
+                                  pos=(200, 140),
+                                  width=200,
+                                  height=30,
+                                  write_tab=False)
         self.add_widget(self.password)
 
         # login button
-        self.login = Button(text = "Login",
-                            size_hint = (None, None),
-                            width = 200,
-                            height = 30,
-                            background_color = (1, 0, 0, 1),
-                            pos = (120, 75)
-                            )
+        self.login = Button(text="Login",
+                            size_hint=(None, None),
+                            width=200,
+                            height=30,
+                            background_color=(1, 0, 0, 1),
+                            pos=(120, 75))
         self.add_widget(self.login)
-        self.login.bind(on_press = self.loginFunction)
+        self.login.bind(on_press=self.loginFunction)
 
         # register button
-        self.login = Button(text = "Register",
-                            size_hint = (None, None),
-                            width = 200,
-                            height = 30,
-                            background_normal = '',
-                            background_color = (1, 0, 0, 1),
-                            pos = (120, 25)
-                            )
+        self.login = Button(text="Register",
+                            size_hint=(None, None),
+                            width=200,
+                            height=30,
+                            background_normal='',
+                            background_color=(1, 0, 0, 1),
+                            pos=(120, 25))
         self.add_widget(self.login)
         self.login.bind(on_press=self.register)
 
+    # method used to log in into application
     def loginFunction(self, object):
         # reading credentials from file
         file = open("./credentials.txt", "rb")
@@ -94,21 +96,21 @@ class Login(FloatLayout):
             self.clear_widgets()
             self.add_widget(MainScreen(self.fernetGenerator, self.key))
         else:
-            closeButton = Button(text = 'Close',
-                                 size_hint = (None, None),
-                                 width = 100,
-                                 height = 30,
-                                 pos = (0, 50))
-            information = Popup(title = "Wrong username or password",
+            closeButton = Button(text='Close',
+                                 size_hint=(None, None),
+                                 width=100,
+                                 height=30,
+                                 pos=(0, 50))
+            information = Popup(title="Wrong username or password",
                                 auto_dismiss=False,
-                                content = closeButton,
-                                size_hint = (None, None),
-                                width = 230,
-                                height = 100,
-                                )
-            closeButton.bind(on_press = information.dismiss)
+                                content=closeButton,
+                                size_hint=(None, None),
+                                width=230,
+                                height=100)
+            closeButton.bind(on_press=information.dismiss)
             information.open()
 
+    # metohd preparing view for registration
     def register(self, object):
         # empty username or password
         if len(self.username.text) == 0 or len(self.password.text) == 0:
@@ -125,28 +127,28 @@ class Login(FloatLayout):
 
         else:
             # register information
-            informationLayout = GridLayout(rows = 4, padding = 5)
-            textLabel = Label(text = "You are trying to create new account.If it is your first time\nlogging into "
-                                     "application click Continue, otherwise click Cancel.\nCreating new account while "
-                                     "having passwords wrote up\nin the memory will erase them!",
-                              size_hint = (1, 1),
-                              pos = (500, 120),
-                              )
-            continueButton = Button(text = "Continue")
+            informationLayout = GridLayout(rows=4, padding=5)
+            textLabel = Label(text="You are trying to create new account.If it is your first time\nlogging into "
+                                   "application click Continue, otherwise click Cancel.\nCreating new account while "
+                                   "having passwords wrote up\nin the memory will erase them!",
+                              size_hint=(1, 1),
+                              pos=(500, 120))
+            continueButton = Button(text="Continue")
             cancelButton = Button(text="Cancel")
 
             informationLayout.add_widget(textLabel)
             informationLayout.add_widget(continueButton)
             informationLayout.add_widget(cancelButton)
 
-            information = Popup(title = "Warning!",
-                                content = informationLayout)
+            information = Popup(title="Warning!",
+                                content=informationLayout)
             information.open()
 
-            cancelButton.bind(on_press = information.dismiss)
-            continueButton.bind(on_press = information.dismiss)
+            cancelButton.bind(on_press=information.dismiss)
+            continueButton.bind(on_press=information.dismiss)
             continueButton.bind(on_press=self.createNewAccount)
 
+    # method saving new account into file
     def createNewAccount(self, object):
         # writing credentials to file
         # but first some password hashing and preparing encoding
--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
 from kivy.config import Config
+
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '450')
 Config.set('graphics', 'height', '320')
@@ -19,6 +20,7 @@ kivy.require('2.1.0')
 
 
 class MainScreen(GridLayout):
+    # constructor in which main layouts are prepared
     def __init__(self, fernetGenerator, key):
         super(MainScreen, self).__init__()
         # declaring elements
@@ -40,89 +42,103 @@ class MainScreen(GridLayout):
         self.spacing = (10, 10)
 
         # button for addind new records
-        self.addData = Button(text = "Add data",
-                              size_hint = (0.2, None),
-                              #width = 70,
-                              height = 30,
-                              background_normal = '',
-                              background_color = (1, 0, 0, 1),
-                              )
-        self.addData.bind(on_press = self.addNewLoginCredentials)
+        self.addData = Button(text="Add data",
+                              size_hint=(0.2, None),
+                              # width = 70,
+                              height=30,
+                              background_normal='',
+                              background_color=(1, 0, 0, 1))
+        self.addData.bind(on_press=self.addNewLoginCredentials)
         self.add_widget(self.addData)
 
         # preparing label for column with details about account
         self.addData = Label(text="Details",
-                             size_hint = (0.5, None),
+                             size_hint=(0.5, None),
                              width=90,
                              height=30,
-                             underline=True
-                             #pos=(120, 25)
-                              )
+                             underline=True)
         self.add_widget(self.addData)
 
-        self.savedAccountsLayout = GridLayout(cols = 1,
-                                              spacing = (10, 10),
-                                              size_hint = (0.2, 1)
-                                              )
-        self.showAccountDetailsLayout = FloatLayout(size_hint = (0.5, 1)
-                                                    )
-
+        # preparing layout for accounts list
+        self.savedAccountsLayout = GridLayout(cols=1,
+                                              spacing=(10, 10),
+                                              size_hint=(0.2, 1))
+        # preparing layout for account details
+        self.showAccountDetailsLayout = FloatLayout(size_hint=(0.5, 1))
         self.showSavedCredentials()
-        self.add_widget(self.root)
-        self.add_widget(self.savedAccountsLayout)
 
-        informationLabel = Label(text = "Choose account to see credentials.",
-                                 size_hint = (0.5, 0.5),
-                                 pos_hint = {'x': 0.28, 'y': 0.6})
-        logoutButton = Button(text = "Logout",
-                              size_hint = (0.3, 0.1))
+        # add scroll bar to accounts list
+        self.root = ScrollView(do_scroll_x=False,
+                               do_scroll_y=True,
+                               size_hint=(0.2, 1),
+                               bar_color=(1, 1, 1, 1),
+                               # effect_cls = 'ScrollEffect'
+                               )
+        self.root.add_widget(self.savedAccountsLayout)
+        self.add_widget(self.root)
+        # self.add_widget(self.savedAccountsLayout)
+        # self.savedAccountsLayout.add_widget(self.root)
+
+        # friendly information to choose one account from the list
+        informationLabel = Label(text="Choose account to see credentials.",
+                                 size_hint=(0.5, 0.5),
+                                 pos_hint={'x': 0.28, 'y': 0.6})
+        logoutButton = Button(text="Logout",
+                              size_hint=(0.3, 0.1))
         self.showAccountDetailsLayout.add_widget(informationLabel)
-        #self.showAccountDetailsLayout.add_widget(logoutButton)
+        # self.showAccountDetailsLayout.add_widget(logoutButton)
         self.add_widget(self.showAccountDetailsLayout)
 
+    # method where view for adding new data is prepared
     def addNewLoginCredentials(self, object):
         # popup where user can add new credentials
+        # preparing layout
         addingLayout = GridLayout(rows=6, padding=5)
         addingLayout.spacing = (7, 7)
+
+        # where credential is used
         self.WhereToUse = TextInput(multiline=False,
                                     hint_text="Where to use credentials",
                                     size_hint=(1, 1),
                                     pos=(200, 200),
-                                    write_tab = False
-                                    )
+                                    write_tab=False)
 
+        # text input for username
         self.username = TextInput(multiline=False,
-                                  hint_text = "Username",
+                                  hint_text="Username",
                                   size_hint=(1, 1),
                                   pos=(200, 200),
-                                  write_tab=False
-                                  )
+                                  write_tab=False)
 
+        # text input for password
         self.password = TextInput(multiline=False,
                                   hint_text="Password",
                                   size_hint=(1, 1),
                                   pos=(200, 200),
-                                  write_tab=False
-                                  )
+                                  write_tab=False)
 
+        # text input for additional description
         self.description = TextInput(multiline=True,
                                      hint_text="Description",
                                      size_hint=(1, 1),
                                      pos=(200, 200),
-                                     write_tab=False
-                                     )
+                                     write_tab=False)
 
-        self.addButton = Button(text ="Save",
-                                size_hint = (1, 1),
-                                background_normal = '',
-                                background_color = (1, 0, 0, 1),
-                                pos = (120, 25))
+        # button for saving information
+        self.addButton = Button(text="Save",
+                                size_hint=(1, 1),
+                                background_normal='',
+                                background_color=(1, 0, 0, 1),
+                                pos=(120, 25))
+
+        # button to reject changes
         self.cancelButton = Button(text="Cancel",
-                                   size_hint = (1, 1),
+                                   size_hint=(1, 1),
                                    background_normal='',
                                    background_color=(1, 0, 0, 1),
                                    pos=(120, 25))
 
+        # adding widgets
         addingLayout.add_widget(self.WhereToUse)
         addingLayout.add_widget(self.username)
         addingLayout.add_widget(self.password)
@@ -130,28 +146,32 @@ class MainScreen(GridLayout):
         addingLayout.add_widget(self.addButton)
         addingLayout.add_widget(self.cancelButton)
 
+        # popup when data is succesfully added
         self.addNewInformation = Popup(title="Adding new credentials",
-                                       content = addingLayout,
-                                       size_hint = (0.52, 1))
+                                       content=addingLayout,
+                                       size_hint=(0.52, 1))
 
-        self.addButton.bind(on_press = self.saveCredentials)
-        self.cancelButton.bind(on_press = self.addNewInformation.dismiss)
+        self.addButton.bind(on_press=self.saveCredentials)
+        self.cancelButton.bind(on_press=self.addNewInformation.dismiss)
 
         self.addNewInformation.open()
 
+    # methow where new data are saved into a file
     def saveCredentials(self, object):
         # checking credentials and saving them to file
-        if len(self.WhereToUse.text) != 0 and self.WhereToUse.text != " " and\
-                len(self.username.text) != 0 and self.username != " " and\
+        if len(self.WhereToUse.text) != 0 and self.WhereToUse.text != " " and \
+                len(self.username.text) != 0 and self.username != " " and \
                 len(self.password.text) != 0 and self.password != " ":
 
+            # when description was empty
             if len(self.description.text) == 0:
                 self.description.text = "empty"
 
-            #salt = bcrypt.gensalt()
+            # encoding password
             password = Fernet(self.fernetGenerator)
             password = password.encrypt(self.password.text.encode())
 
+            # saving data to a file
             file = open("./credentials.txt", "ab")
             file.write(bytes("\n", 'ASCII'))
             file.write(bytes(self.WhereToUse.text, 'ASCII'))
@@ -169,20 +189,22 @@ class MainScreen(GridLayout):
             self.showSavedCredentials()
         # if credentials are not good show another popup
         else:
-            newCredentialsLayoutError = GridLayout(rows = 4)
-            info = Label(text = "Descripton must not be empty.\n"
-                                "Username must not be empty.\n"
-                                "Password must not be empty.")
-            button = Button(text = "OK")
+            newCredentialsLayoutError = GridLayout(rows=4)
+            info = Label(text="Descripton must not be empty.\n"
+                              "Username must not be empty.\n"
+                              "Password must not be empty.")
+            button = Button(text="OK")
 
             newCredentialsLayoutError.add_widget(info)
             newCredentialsLayoutError.add_widget(button)
 
+            # show popup with warning about data
             information = Popup(title="Warning",
                                 content=newCredentialsLayoutError)
             information.open()
-            button.bind(on_press = information.dismiss)
+            button.bind(on_press=information.dismiss)
 
+    # method showing saved data
     def showSavedCredentials(self):
         # reading data from file
         file = open("./credentials.txt", "rb")
@@ -195,71 +217,77 @@ class MainScreen(GridLayout):
         # iterating through data and selecting WhereToUse property to use it as button text
         for index, element in enumerate(fileContent[4:]):
             if index % 4 == 0:
-                #self.WhereToUse = str(credentialsList[0 + index]).split("'")[1]
+                # getting items from list to use them in detailed view
+                # self.WhereToUse = str(credentialsList[0 + index]).split("'")[1]
                 self.username = str(credentialsList[1 + index]).split("'")[1]
                 self.password = str(credentialsList[2 + index]).split("'")[1]
+
+                # decoding password
                 decryptedPassword = Fernet(self.fernetGenerator)
                 decryptedPassword = decryptedPassword.decrypt(bytes(self.password, 'ASCII')).decode()
 
+                # when description was left empty
                 if str(credentialsList[3 + index]).split("'")[1] == "empty":
                     self.description = ""
                 else:
                     self.description = str(credentialsList[3 + index]).split("'")[1]
                 title = str(element).split("'")[1]
-                self.listPosition = Button(text = title,
-                                           size_hint = (0.5, None),
-                                           #width = 70,
-                                           height = 30,
-                                           background_normal = '',
-                                           background_color = (0.96, 0.71, 0, 1)
-                                           )
+
+                # adding buttons with title
+                self.listPosition = Button(text=title,
+                                           size_hint=(None, None),
+                                           width=125,
+                                           height=30,
+                                           background_normal='',
+                                           background_color=(0.96, 0.71, 0, 1))
+
+                # adding username, password and description to details
                 self.listPosition.bind(on_press=partial(self.showAccountInformation, self.username, decryptedPassword,
                                                         self.description))
                 self.savedAccountsLayout.add_widget(self.listPosition)
-        self.root = ScrollView(do_scroll_x = False,
-                               width=70,
-                               height=30,
-                               bar_color = (1, 1, 1, 1),
-                               effect_cls = 'ScrollEffect'
-                               )
-        self.root.add_widget(self.savedAccountsLayout)
+        # self.root = ScrollView(do_scroll_y = False,
+        #                        #size_hint = (0.2, 1),
+        #                        bar_color = (1, 1, 1, 1),
+        #                        #effect_cls = 'ScrollEffect'
+        #                        )
+        # self.root.add_widget(self.savedAccountsLayout)
 
+    # method preparing view to show when button with account is clicked
     def showAccountInformation(self, username, password, description, object):
+        # clearing view from any widgets
         self.showAccountDetailsLayout.clear_widgets()
-        usernameLabel = Label(text = "Username:",
-                              size_hint = (0.5, None),
-                              pos_hint = {'x': 0, 'y': 0.7}
-                              )
-        passwordLabel = Label(text = "Password:",
+
+        # preparing labels for username, password and description
+        usernameLabel = Label(text="Username:",
                               size_hint=(0.5, None),
-                              pos_hint={'x': 0, 'y': 0.5}
-                              )
-        descriptionLabel = Label(text = "Description:",
+                              pos_hint={'x': 0, 'y': 0.7})
+        passwordLabel = Label(text="Password:",
+                              size_hint=(0.5, None),
+                              pos_hint={'x': 0, 'y': 0.5})
+        descriptionLabel = Label(text="Description:",
                                  size_hint=(0.5, None),
-                                 pos_hint={'x': 0, 'y': 0.3}
-                                 )
-        usernameValue = TextInput(text = "{}".format(username),
-                                  readonly = True,
-                                  multiline = False,
+                                 pos_hint={'x': 0, 'y': 0.3})
+
+        # preparing text inputs to display data
+        usernameValue = TextInput(text="{}".format(username),
+                                  readonly=True,
+                                  multiline=False,
                                   size_hint=(0.49, 0.12),
                                   pos_hint={'x': 0.5, 'y': 0.83})
-        passwordValue = TextInput(text = "{}".format(password),
-                                  readonly = True,
-                                  multiline = False,
+        passwordValue = TextInput(text="{}".format(password),
+                                  readonly=True,
+                                  multiline=False,
                                   size_hint=(0.49, 0.12),
-                                  pos_hint={'x': 0.5, 'y': 0.63}
-                                  )
-        descriptionValue = TextInput(text = "{}".format(description),
-                                     readonly = True,
-                                     multiline = True,
+                                  pos_hint={'x': 0.5, 'y': 0.63})
+        descriptionValue = TextInput(text="{}".format(description),
+                                     readonly=True,
+                                     multiline=True,
                                      size_hint=(0.49, 0.2),
-                                     pos_hint={'x': 0.5, 'y': 0.35}
-                                     )
-        closeButton = Button(text = "Close",
+                                     pos_hint={'x': 0.5, 'y': 0.35})
+        closeButton = Button(text="Close",
                              size_hint=(0.3, 0.2),
-                             pos_hint={'x': 0.3, 'y': 0.1}
-                             )
-        closeButton.bind(on_press = self.clearDetails)
+                             pos_hint={'x': 0.3, 'y': 0.1})
+        closeButton.bind(on_press=self.clearDetails)
 
         self.showAccountDetailsLayout.add_widget(usernameLabel)
         self.showAccountDetailsLayout.add_widget(usernameValue)
@@ -272,17 +300,20 @@ class MainScreen(GridLayout):
 
         self.showAccountDetailsLayout.add_widget(closeButton)
 
+    # clearing detailed view from any widgets and showing friendly information
     def clearDetails(self, ojb):
+        # clearing main window from any widgets connected to detailed view
         self.showAccountDetailsLayout.clear_widgets()
         informationLabel = Label(text="Choose account to see credentials.",
                                  size_hint=(0.5, 0.5),
-                                 pos_hint={'x': 0.28, 'y': 0.6}
-                                 )
+                                 pos_hint={'x': 0.28, 'y': 0.6})
         self.showAccountDetailsLayout.add_widget(informationLabel)
 
 
 fernetGenerator = b'6vEhzwGrIgziXF4I7GPRKOZTlXGR-DpZCRg1bkiEmP0=\n'
 key = b'\xe6S\x95q\x91\xf8:Y\t\x185nH\x84\x98\xfd\n'
+
+
 class MyApp(App):
     def build(self):
         self.title = "Password Manager"
